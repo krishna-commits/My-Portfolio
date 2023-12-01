c6dea8c83adb437c9c1de332ba5298a7bf7bfc29
Krishna <neupane.krishna33@gmail.com>
2023-12-01 13:13:53 +0545

    add clarity

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,14 +7,22 @@ export default function Document() {
             gtag('js', new Date());
             
             gtag('config', 'G-KNH33H1VPP');`;
+            const clarity = `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "jyzxip5knt");`
+
             return (
               <Html lang="en">
+
       <Head />
       <body>
         <Main />
         <NextScript />
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-KNH33H1VPP"></script>
         <script async dangerouslySetInnerHTML={{ __html: googleTag }} />
+        <script async dangerouslySetInnerHTML={{ __html: clarity }} />
       </body>
     </Html>
   );

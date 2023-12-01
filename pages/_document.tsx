a5ff0d583f50c7af9916b5f536e9421fa38dd047
Krishna <neupane.krishna33@gmail.com>
2023-12-01 11:43:10 +0545

    Add Google tag for tracking

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,24 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const hotjar = `
+  <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KNH33H1VPP"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KNH33H1VPP');
+</script>
+  `;
   return (
     <Html lang="en">
       <Head />
       <body>
         <Main />
         <NextScript />
+        <script defer src={hotjar} />
       </body>
     </Html>
   );

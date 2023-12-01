2d48af9d4c4855ed8cf5339a79d8918e59c5d703
Krishna <neupane.krishna33@gmail.com>
2023-12-01 11:58:59 +0545

    added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,24 +1,20 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  const hotjar = `
-            <!-- Google tag (gtag.js) -->
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-KNH33H1VPP"></script>
-          <script>
+  const googleTag = `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-
-            gtag('config', 'G-KNH33H1VPP');
-          </script>
-            `;
-  return (
-    <Html lang="en">
+            
+            gtag('config', 'G-KNH33H1VPP');`;
+            return (
+              <Html lang="en">
       <Head />
       <body>
         <Main />
         <NextScript />
-        <script defer dangerouslySetInnerHTML={{ __html: hotjar }} />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-KNH33H1VPP"></script>
+        <script async dangerouslySetInnerHTML={{ __html: googleTag }} />
       </body>
     </Html>
   );

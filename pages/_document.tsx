5eab18e24c094ce8c28ef59f52a57405538b6931
Krishna <neupane.krishna33@gmail.com>
2023-12-01 11:50:59 +0545

    added google

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,23 +2,23 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   const hotjar = `
-  <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-KNH33H1VPP"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+            <!-- Google tag (gtag.js) -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-KNH33H1VPP"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-  gtag('config', 'G-KNH33H1VPP');
-</script>
-  `;
+            gtag('config', 'G-KNH33H1VPP');
+          </script>
+            `;
   return (
     <Html lang="en">
       <Head />
       <body>
         <Main />
         <NextScript />
-        <script defer src={hotjar} />
+        <script defer dangerouslySetInnerHTML={{ __html: hotjar }} />
       </body>
     </Html>
   );

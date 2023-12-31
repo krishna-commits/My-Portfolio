7a1ab1fc32836b3b9e8da11632898edc82a81f8f
Krishna <neupane.krishna33@gmail.com>
2023-12-31 14:42:15 +0545

    test

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -1,22 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { EmailTemplate } from './email-template';
 import { Resend } from 'resend';
-import { NextRequest, NextResponse } from 'next/server';
-import MessageUsEmail from './email-template';
 
-const resend = new Resend('re_RJLeeJN9_FDm74qQmuhFU5RKGrxgQxiia');
+const resend = new Resend('re_C9BeaK7X_2dTLr4b1juLDDgtbPpq6XqcN');
 
-export async function POST(req: NextRequest) {
-  const { name, email, message, phone, country, company } = await req.json();
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { data, error } = await resend.emails.send({
+    from: 'Acme <onboarding@resend.dev>',
+    to: ['delivered@resend.dev'],
+    subject: 'Hello world',
+    react: EmailTemplate({ firstName: 'John' }),
+  });
 
-  try {
-    const data = await resend.emails.send({
-      from: `${name} <onboarding@neupanekrishna.com.np>`, // your verified domain
-      to: ['neupane.krishna33@gmail.com'],  // the email address you want to send a message
-      subject: `${name} has a message!`,
-      react: MessageUsEmail({ name, email, message, phone, country, company }),
-    });
-
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json({ error });
+  if (error) {
+    return res.status(400).json(error);
   }
-}
+
+  res.status(200).json(data);
+};

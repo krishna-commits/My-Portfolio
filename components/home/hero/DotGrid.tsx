3dcb20d6cc9183f84d3d018eed4fb4b36b8828c8
Krishna <neupane.krishna33@gmail.com>
2024-01-02 17:09:55 +0545

    added the animation

--- components/home/hero/DotGrid.tsx
+++ components/home/hero/DotGrid.tsx
@@ -2,66 +2,70 @@ import { useEffect } from "react";
 import styles from "./dotgrid.module.scss";
 import anime from "animejs";
 import Image from "next/image";
+import Lottie from "react-lottie-player";
+import { lottieJSON } from "@/constants/lottie";
 
 export const DotGrid = () => {
-  const GRID_WIDTH = 25;
-  const GRID_HEIGHT = 20;
+  // const GRID_WIDTH = 25;
+  // const GRID_HEIGHT = 20;
 
-  const animateDots = (startIndex) => {
-    anime({
-      targets: ".dot-point",
-      scale: [
-        { value: 1.35, easing: "easeOutSine", duration: 250 },
-        { value: 1, easing: "easeInOutQuad", duration: 500 },
-      ],
-      translateY: [
-        { value: -15, easing: "easeOutSine", duration: 250 },
-        { value: 1, easing: "easeInOutQuad", duration: 500 },
-      ],
-      opacity: [
-        { value: 0.7, easing: "easeOutSine", duration: 250 },
-        { value: 0.35, easing: "easeInOutQuad", duration: 500 },
-      ],
-      delay: anime.stagger(100, {
-        grid: [GRID_WIDTH, GRID_HEIGHT],
-        from: startIndex,
-      }),
-    });
-  };
+  const lotieJSON:any='https://assets5.lottiefiles.com/packages/lf20_w51pcehl.json'
+  console.log(lotieJSON)
+  // const animateDots = (startIndex) => {
+  //   anime({
+  //     targets: ".dot-point",
+  //     scale: [
+  //       { value: 1.35, easing: "easeOutSine", duration: 250 },
+  //       { value: 1, easing: "easeInOutQuad", duration: 500 },
+  //     ],
+  //     translateY: [
+  //       { value: -15, easing: "easeOutSine", duration: 250 },
+  //       { value: 1, easing: "easeInOutQuad", duration: 500 },
+  //     ],
+  //     opacity: [
+  //       { value: 0.7, easing: "easeOutSine", duration: 250 },
+  //       { value: 0.35, easing: "easeInOutQuad", duration: 500 },
+  //     ],
+  //     delay: anime.stagger(100, {
+  //       grid: [GRID_WIDTH, GRID_HEIGHT],
+  //       from: startIndex,
+  //     }),
+  //   });
+  // };
 
-  useEffect(() => {
-    animateDots(0);
-  }, []);
+  // useEffect(() => {
+  //   animateDots(0);
+  // }, []);
 
-  const handleDotClick = (e) => {
-    const startIndex = parseInt(e.target.dataset.index);
-    animateDots(startIndex);
-  };
+  // const handleDotClick = (e) => {
+  //   const startIndex = parseInt(e.target.dataset.index);
+  //   animateDots(startIndex);
+  // };
 
-  const dots = [];
+  // const dots = [];
 
-  let index = 0;
+  // let index = 0;
 
-  for (let i = 0; i < GRID_WIDTH; i++) {
-    for (let j = 0; j < GRID_HEIGHT; j++) {
-      dots.push(
-        <div
-          onClick={handleDotClick}
-          className={styles.dotWrapper}
-          data-index={index}
-          key={`${i}-${j}`}
-        >
-          <div className={`${styles.dot} dot-point`} data-index={index} />
-        </div>
-      );
-      index++;
-    }
-  }
+  // for (let i = 0; i < GRID_WIDTH; i++) {
+  //   for (let j = 0; j < GRID_HEIGHT; j++) {
+  //     dots.push(
+  //       <div
+  //         onClick={handleDotClick}
+  //         className={styles.dotWrapper}
+  //         data-index={index}
+  //         key={`${i}-${j}`}
+  //       >
+  //         <div className={`${styles.dot} dot-point`} data-index={index} />
+  //       </div>
+  //     );
+  //     index++;
+  //   }
+  // }
 
   return (
     <div
-      style={{ gridTemplateColumns: `repeat(${GRID_WIDTH}, 1fr)` }}
-      className={styles.dotGrid}
+      // style={{ gridTemplateColumns: `repeat(${GRID_WIDTH}, 1fr)` }}
+      // className={styles.dotGrid}
     >
        {/* <Image
           className={styles.modalImage}
@@ -71,16 +75,16 @@ export const DotGrid = () => {
           style={{ maxWidth: '100%', height: 'auto' }}
           alt={`An image of the ${title} project.`}
         /> */}
+{/* <div style={{ marginLeft: 'auto', width: '50%', height: 600 }}></div> */}
+<Lottie
+      loop
+      animationData={lottieJSON}
+      play
+      style={{ marginLeft: 'auto', width: 600, height: 500 }}
+    />
         
-      {/* <Image
-      className="right-side"
-        src="/project-imgs/laptop.svg"
-        width={700}
-        height={700}
-        alt="Picture "
-
-      /> */}
-      {dots.map((dot) => dot)}
+    
+      {/* {dots.map((dot) => dot)} */}
     </div>
   );
 };
--- components/home/hero/Hero.tsx
+++ components/home/hero/Hero.tsx
@@ -8,56 +8,66 @@ import Image from "next/image";
 export const Hero = () => {
   return (
     <section className={`section-wrapper ${styles.hero}`}>
-      <div className={styles.copyWrapper}>
-        <Reveal>
-          <h1 className={styles.title}>
-            Krishna Neupane<span>.</span>
-          </h1>
-        </Reveal>
-        <Reveal>
-          <h2 className={styles.subTitle}>
-            I&apos;m a <span><ReactTypingEffect
-              text={["DevSecOps Engineer.", "DevOps Enthusiast.", "Cybersecurity Learner.", "Aspiring Researcher."]}
-              speed={40}
-              eraseSpeed={40}
-              eraseDelay={900}
-              typingDelay={300}
-            /></span>
-          </h2>
-        </Reveal>
-        <Reveal>
-          <p className={styles.aboutCopy}>
-            {/* Welcome To My Portfolio Site */}
-            <br />
-            Let&apos;s Connect !!!
-            <br />
-            <a
-    id="cy-effective-orcid-url"
-    className="underline"
-     href="https://orcid.org/0009-0004-7787-1573"
-     target="orcid.widget"
-     rel="me noopener noreferrer"
-     >
-      <br />
-     <img
-        src="https://orcid.org/sites/default/files/images/orcid_16x16.png"
-        alt="ORCID iD icon"/>
-      https://orcid.org/0009-0004-7787-1573
-    </a>
-          </p>
-        
-        </Reveal>
-        <Reveal>
-          <OutlineButton
-          
-            onClick={() => document.getElementById("contact")?.scrollIntoView()}
-          >
-            Contact me
-          </OutlineButton>
-        </Reveal>
+      <div className={styles.contentWrapper}>
+        <div className={styles.leftContent}>
+          <Reveal>
+            <h1 className={styles.title}>
+              Krishna Neupane<span>.</span>
+            </h1>
+          </Reveal>
+          <Reveal>
+            <h2 className={styles.subTitle}>
+              I&apos;m a{" "}
+              <span>
+                <ReactTypingEffect
+                  text={[
+                    "DevSecOps Engineer.",
+                    "DevOps Enthusiast.",
+                    "Cybersecurity Learner.",
+                    "Aspiring Researcher.",
+                  ]}
+                  speed={40}
+                  eraseSpeed={40}
+                  eraseDelay={900}
+                  typingDelay={300}
+                />
+              </span>
+            </h2>
+          </Reveal>
+          <Reveal>
+            <p className={styles.aboutCopy}>
+              {/* Welcome To My Portfolio Site */}
+              <br />
+              Let&apos;s Connect !!!
+              <br />
+              {/* <a
+                id="cy-effective-orcid-url"
+                className="underline"
+                href="https://orcid.org/0009-0004-7787-1573"
+                target="orcid.widget"
+                rel="me noopener noreferrer"
+              >
+                <br />
+                <img
+                  src="https://orcid.org/sites/default/files/images/orcid_16x16.png"
+                  alt="ORCID iD icon"
+                />
+                https://orcid.org/0009-0004-7787-1573
+              </a> */}
+            </p>
+          </Reveal>
+          <Reveal>
+            <OutlineButton
+              onClick={() => document.getElementById("contact")?.scrollIntoView()}
+            >
+              Contact me
+            </OutlineButton>
+          </Reveal>
+        </div>
       </div>
-    
-      <DotGrid />
+      <div className={`${styles.rightContent} ${styles.floatingAnimation}`}>
+        <DotGrid />
+      </div>
     </section>
   );
 };
